00456afa9e02faabb82c8be3cd086eafd642001f
Marc Tardif <marc@taram.ca>
2026-04-19 21:21:24 -0400

    Migrate certbot to taracert

--- certbot/deploy-hook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Configuration
-LE_DIR=/etc/letsencrypt
-
-# Copy primary private key and full chain to live directory
-cp ${LE_DIR}/live/${SERVER_HOSTNAME}/privkey.pem ${LE_DIR}/live/privkey.pem
-cp ${LE_DIR}/live/${SERVER_HOSTNAME}/fullchain.pem ${LE_DIR}/live/fullchain.pem
